7cad1d82dc17297bd486d45f048a714d935a5b8c
Alejandrogonzales99 <43149431+Alejandrogonzales99@users.noreply.github.com>
2018-09-16 00:20:30 -0400

    Update INVENTARIO.java

--- eclipse-workspace/Proyecto_Progra_2/src/INVENTARIO.java
+++ eclipse-workspace/Proyecto_Progra_2/src/INVENTARIO.java
@@ -1,4 +1,70 @@
-
 public class INVENTARIO {
 
+
+
+String PiezasGrandes, PiezasMedianas, PiezasPequeñas, Metales, NoMetales;
+	int Cantidad;
+	
+public void Inventario(String piezasgrandes, String piezasmedianas, String piezaspequeñas, String metales, String no_metales)
+{
+	
+	PiezasGrandes=piezasgrandes;
+	PiezasMedianas=piezasmedianas;
+	PiezasPequeñas=piezaspequeñas;
+	Metales=metales;
+	NoMetales=no_metales;
 }
+
+public String getPiezasGrandes() {
+	return PiezasGrandes;
+}
+
+public void setPiezasGrandes(String piezasGrandes) {
+	PiezasGrandes = piezasGrandes;
+}
+
+public String getPiezasMedianas() {
+	return PiezasMedianas;
+}
+
+public void setPiezasMedianas(String piezasMedianas) {
+	PiezasMedianas = piezasMedianas;
+}
+
+public String getPiezasPequeñas() {
+	return PiezasPequeñas;
+}
+
+public void setPiezasPequeñas(String piezasPequeñas) {
+	PiezasPequeñas = piezasPequeñas;
+}
+
+public String getMetales() {
+	return Metales;
+}
+
+public void setMetales(String metales) {
+	Metales = metales;
+}
+
+public String getNoMetales() {
+	return NoMetales;
+}
+
+public void setNoMetales(String noMetales) {
+	NoMetales = noMetales;
+}
+
+public int getCantidad() {
+	return Cantidad;
+}
+
+public void setCantidad(int cantidad) {
+	Cantidad = cantidad;
+}
+public String LeerDatosInventario()
+{
+	return (getPiezasGrandes() + "/" +getPiezasMedianas() + "/" + getPiezasPequeñas() + "/" + getNoMetales() + "/" +getCantidad());
+}
+}
+
