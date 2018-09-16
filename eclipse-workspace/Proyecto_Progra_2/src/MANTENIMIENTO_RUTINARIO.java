f8b6931ff57f86fe309db4faf9aec0a5e80ad88b
robiiespejo99 <43149629+robiiespejo99@users.noreply.github.com>
2018-09-16 03:36:57 -0400

    Update MANTENIMIENTO_RUTINARIO.java

--- eclipse-workspace/Proyecto_Progra_2/src/MANTENIMIENTO_RUTINARIO.java
+++ eclipse-workspace/Proyecto_Progra_2/src/MANTENIMIENTO_RUTINARIO.java
@@ -1,4 +1,25 @@
+import java.sql.SQLException;
+import java.util.ArrayList;
+import java.util.InputMismatchException;
+import java.util.Scanner;
 
 public class MANTENIMIENTO_RUTINARIO {
+  
+  int op1;
+  ArrayList<Maquinas> maquinaria=new ArrayList<Maquinas>();
+  ArrayList<Inventario> inventario=new ArrayList<Inventario>();
+  ArrayList<Trabajadores> trabajadores=new ArrayList<Trabajadores>();
+  
+  CONECTAR_BD e1=new CONECTAR_BD();
+  Scanner leer=new Scanner(System.in); 
+  
+  public void MenuRutinario() throws InputMismatchException {
+    
+    
+    do
+    {
+      try
+      {
+        System.out.println
 
 }
