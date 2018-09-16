33e96a01504d944b54f57f5014b1def77adb39a8
robiiespejo99 <43149629+robiiespejo99@users.noreply.github.com>
2018-09-16 03:29:16 -0400

    Update MANTENIMIENTO_PREVENTIVO.java

--- eclipse-workspace/Proyecto_Progra_2/src/MANTENIMIENTO_PREVENTIVO.java
+++ eclipse-workspace/Proyecto_Progra_2/src/MANTENIMIENTO_PREVENTIVO.java
@@ -1,4 +1,37 @@
+import java.sql.SQLException;
+import java.util.ArrayList;
+import java.util.InputMismatchException;
+import java.util.Scanner;
 
 public class MANTENIMIENTO_PREVENTIVO {
+  
+  int op;
+  
+  ArrayList<Maquinas> maquinaria= new ArrayList<Maquinas>();
+  ArrayList<Inventario> inventario=new ArrayList<Inventario>();
+  
+  CONECTAR_BD z1=new CONECTAR_BD();
+  Scanner leer=new Scanner(System.in);
+  
+  public void MenuPreventivo() throws InputMismatchException {
+    
+    
+    Scanner leer=new Scanner(System.in);
+    
+    do
+    {
+      try
+      {
+        System.out.println("*****Menu De Mantenimiento Preventivo*****");
+        System.out.println("1> Emergencia Mecanico");
+        System.out.println("2> Emergencia Electricista");
+        System.out.println("3> Emergencia Soldador");
+        System.out.println("4> Emergencia Instrumentista");
+        System.out.println("5> SALIR");
+        
+        System.out.println("Elija su opcion");
+        op=leer.nextInt();
+        switch(op)
+        {
 
 }
