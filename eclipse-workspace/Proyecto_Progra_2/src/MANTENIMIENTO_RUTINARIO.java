ed0e0f1ae52ddc7e6c4587896ac13e724c0d1beb
robiiespejo99 <43149629+robiiespejo99@users.noreply.github.com>
2018-09-17 01:39:01 -0400

    Update MANTENIMIENTO_RUTINARIO.java

--- eclipse-workspace/Proyecto_Progra_2/src/MANTENIMIENTO_RUTINARIO.java
+++ eclipse-workspace/Proyecto_Progra_2/src/MANTENIMIENTO_RUTINARIO.java
@@ -20,6 +20,48 @@ public class MANTENIMIENTO_RUTINARIO {
     {
       try
       {
-        System.out.println
+        System.out.println("*****Menu Mantenimiento Rutinario*****");
+        System.out.println("1> Orden rutinaria Instrumentista");
+        System.out.println("2> Orden RUTINARIA Mecanico");
+        System.out.println("3> Orden rutinaria electricista");
+        System.out.println("4> Orden rutinaria soldador");
+        System.out.println("5> Salir");
+        
+        System.out.println("Elija su opcion");
+        op1=leer.nextInt();
+        switch(op1)
+        {
+          case 1:
+            System.out.println("Ingrese el codigo de 4 enteros");
+            int cod=leer.nextInt();
+            System.out.println("Ingrese la orden del dia");
+            String OrdenDia=leer.next();
+            
+            try {
+              e1.Orden_del_dia(cod, OrdenDia);
+            } catch (SQLException e) {
+              // TODO Auto-generated catch block
+              e.printStackTrace();
+            }
+            
+            break;
+            
+          case 2:
+            System.out.println("Ingrese el codigo de 4 enteros");
+            cod=leer.nextInt();
+            System.out.println("Ingrese la orden del dia");
+            OrdenDia=leer.next();
+            
+            try{
+              e1.Orden_del_dia(cod, OrdenDia);
+            } catch (SQLException e) {
+              // TODO Auto-generated catch block
+				e.printStackTrace();
+            }
+            
+            break;
+            
+            case 3:
+              
 
 }
