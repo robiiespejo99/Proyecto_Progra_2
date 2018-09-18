c2e8e633da983cde17e75adac11ae793d06a71d9
robiiespejo99 <43149629+robiiespejo99@users.noreply.github.com>
2018-09-18 00:31:51 -0400

    Update Trabajadores.java

--- eclipse-workspace/Proyecto_Progra_2/src/Trabajadores.java
+++ eclipse-workspace/Proyecto_Progra_2/src/Trabajadores.java
@@ -1,4 +1,20 @@
 
 public class Trabajadores {
+  String Nombre, Trabajo, OrdenDiaria, PlanApoyo, PlanCautelar;
+  
+  public Trabajadores(String nombre, String trabajo, String orden_diaria, String plan_apoyo, String plan_cautelar)
+  {
+    Nombre=nombre;
+    Trabajo=trabajo;
+    OrdenDiaria=orden_diaria;
+    PlanApoyo=plan_apoyo;
+    PlanCautelar=plan_cautelar;
+  }
+  public String getNombre() {
+    return Nombre;
+  }
+  
+  public void setNombre(String nombre) {
+    Nombre = nombre;
 
 }
